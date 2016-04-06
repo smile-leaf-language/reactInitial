ad8e2659f223f0ee3382c24865ea3940705d9e50
smile-leaf-language <1006231719@qq.com>
2016-04-06 15:17:23 +0800

    webpack react-hot style-loader css-loader less-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
-var path=require("path");
-module.exports={
-  entry:path.resolve(__dirname,'app/index.js'),
-  output:{
-    path:path.resolve(__dirname,'public'),
-    filename:'bundle.js'
-  },
-  module:{
-    loaders:[
-      {
-        test:/\.js$/,
-        loader:'babel-loader'
-      }
+var path = require("path");
+module.exports = {
+    entry: [
+        'webpack/hot/-dev-server',
+        'webpack-dev-server/client?http://localhost:8080/',
+        path.resolve(__dirname, 'app/index.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: ['react-hot','babel'],
+                exclude:path.resolve('_dirname','node_modules')
+            },
+            {
+                test:/\.css/,
+                loader:'style!css'
+            },
+            {
+                test:/\.less/,
+                loader:'style!css!less'
+            }
+        ]
+    }
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
     ]
-  }
 };
